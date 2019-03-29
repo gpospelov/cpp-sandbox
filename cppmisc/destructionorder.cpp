402445aa0c764e58ab48ad67e9bb1122cc98ebc9
Gennady Pospelov <g.pospelov@fz-juelich.de>
2019-03-29 10:10:25 +0100

    More experiments with destruction order

--- cppmisc/destructionorder.cpp
+++ cppmisc/destructionorder.cpp
@@ -59,13 +59,59 @@ public:
     std::unique_ptr<C> m_c;
 };
 
+class Item
+{
+public:
+    Item(std::string model) : m_modelType(model), m_parent(nullptr) {
+        std::cout << "Item() " << m_modelType << std::endl;
+    }
+    virtual ~Item() {
+        std::cout << "~Item() " << m_modelType << std::endl;
+        if (m_parent)
+            m_parent->report_from_child(this);
+    }
+
+    void report_from_child(Item* child) {
+        std::cout << "report_from_child() me: '" << m_modelType << "', child:'" << child->m_modelType << "'" << std::endl;
+    }
+
+    void setItem(Item* item) {
+        std::cout << "setItem" << std::endl;
+        item->setParent(this);
+        m_child.reset(item);
+    }
+
+    void setParent(Item* parent)
+    {
+        std::cout << "setParent: me:'" << m_modelType << "', parent:'" << parent->m_modelType << "'" << std::endl;
+        m_parent = parent;
+    }
+    std::unique_ptr<Item> m_child;
+    std::string m_modelType;
+    Item* m_parent;
+};
+
+class RootItem : public Item{
+public:
+    RootItem() : Item("root") {}
+};
+
+class Node : public Item{
+public:
+    Node() : Item("node") {}
+};
+
 int main()
 {
-    std::cout << "Hello Gennady" << std::endl;
-    std::cout << "--- before construction" << std::endl;
-    std::unique_ptr<Derived> d = std::make_unique<Derived>();
-    std::cout << "--- before destruction" << std::endl;
-    d.reset();
-    std::cout << "--- after destruction" << std::endl;
+//    std::cout << "--- before construction" << std::endl;
+//    std::unique_ptr<Derived> d = std::make_unique<Derived>();
+//    std::cout << "--- before destruction" << std::endl;
+//    d.reset();
+//    std::cout << "--- after destruction" << std::endl;
+
+    auto root = new RootItem;
+    auto node = new Node;
+    root->setItem(node);
+    delete root;
     return 0;
 }
